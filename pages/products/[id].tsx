ba628f1a216f814b6ca2ed7c92fce31757a08bd3
saitogen <saitogen921@gmail.com>
2023-02-16 19:30:15 +0900

    修正箇所不明:一旦休憩

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -18,7 +18,6 @@ export async function getStaticProps({ params }:any) {
 export async function getStaticPaths() {
     const req= await fetch(`http://localhost:3000/products.json`);
     const data=await req.json();
-    
     const paths=data.map((product:any) =>{
         return{
             params:{
